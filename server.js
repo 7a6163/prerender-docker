a14b144193c4c79844065efe1169020476a497ba
Zac <579103+7a6163@users.noreply.github.com>
2025-11-07 15:26:00 +0800

    build: upgrade to Node 24-alpine and add Redis-backed prerender cache

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const prerender = require('prerender');
-const prMemoryCache = require('prerender-memory-cache');
+const redisCache = require('prerender-redis-cache-ng');
 
 const server = prerender({
     chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars', '--disable-dev-shm-usage'],
@@ -9,7 +9,7 @@ const server = prerender({
     chromeLocation: '/usr/bin/chromium-browser'
 });
 
-server.use(prMemoryCache);
+server.use(redisCache);
 server.use(prerender.blacklist());
 server.use(prerender.httpHeaders());
 server.use(prerender.removeScriptTags());
